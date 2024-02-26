04e5fa6ffd51c862cc5dded9bb0e65555d6f885a
diego <dmarques1508@gmail.com>
2024-02-25 22:48:42 -0300

    feat: cadastrando usuário

--- src/pages/SingUp/index.jsx
+++ src/pages/SingUp/index.jsx
@@ -1,13 +1,45 @@
+import { useState } from "react"
+
 import { Container } from "./styles"
 
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+
+import { api } from "../../services/api"
 
 import MyIcon from "../../assets/MyIcon.svg"
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 
 
+
 export function SingUp(){
+
+    const[ name, setName ] = useState("");
+    const[ email, setEmail ] = useState("");
+    const[ password, setPassword ] = useState("");
+
+    const navigate = useNavigate();
+
+
+    async function handleSignUp(event){
+    event.preventDefault();
+        if(!name || !email || !password){
+            return alert("Preencha todos os campos")
+        }
+    api.post("/users", {name, email, password})
+    .then(() => {
+        alert("Usuário cadastrado com sucesso!")
+        navigate("/")
+    })
+    .catch( error => {
+        if(error.response){
+            alert(error.response.data.message)
+        } else{
+            alert("Não foi possível cadastrar")
+        }
+    })
+    }
+
     return(
         <Container>
             <h1 className="nameLogo">
@@ -15,7 +47,7 @@ export function SingUp(){
                 Food Explorer
             </h1>
 
-            <form action="">
+            <form onSubmit={handleSignUp}>
                 
                 <h1>Crie sua conta</h1>
 
@@ -24,6 +56,8 @@ export function SingUp(){
                 <Input
                 placeholder="Exemplo: Maria da Silva"
                 id="UserName"
+                type="text"
+                onChange={e => setName(e.target.value)}
                 />
 
                 <label htmlFor="UserEmail">Email</label>
@@ -31,6 +65,8 @@ export function SingUp(){
                 <Input
                 placeholder="No mínimo 6 caracteres"
                 id="UserEmail"
+                type="text"
+                onChange={e => setEmail(e.target.value)}
                 />
 
                 <label htmlFor="UserPassword">Senha</label>
@@ -38,9 +74,11 @@ export function SingUp(){
                 <Input
                 placeholder="No mínimo 6 caracteres"
                 id="UserPassword"
+                type="password"
+                onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button className="button-criar" text="Criar conta"/>
+                <Button type="submit" className="button-criar" text="Criar conta" />
 
                 <Link to="/">Ja tenho uma conta</Link>
 
