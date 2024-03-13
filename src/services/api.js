216c2049465f085566bda4c4563194e9793cb887
D2Diego <dmarques1508@gmail.com>
2024-03-13 11:32:38 -0300

    fix: base URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://food-explorer-api-wu3e.onrender.com"
+    baseURL: "https://food-explorer-api-dz5z.onrender.com"
 });
